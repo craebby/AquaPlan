0c1668189471b6e8dbf7f9ca4bbdef672ca02650
craebby <craebby@helmsdeep>
2018-01-26 14:21:25 +0100

    Added Delete, Add and Edit Tank functions

--- src/de/craebby/aquaplan/MainApp.java
+++ src/de/craebby/aquaplan/MainApp.java
@@ -3,6 +3,7 @@ package de.craebby.aquaplan;
 import java.io.IOException;
 
 import de.craebby.aquaplan.model.Tank;
+import de.craebby.aquaplan.view.TankEditDialogController;
 import de.craebby.aquaplan.view.TankOverviewController;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -11,6 +12,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -78,6 +80,36 @@ public class MainApp extends Application {
             
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    
+    public boolean showTankEditDialog(Tank tank) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/TankEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            TankEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setTank(tank);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
--- src/de/craebby/aquaplan/view/TankOverviewController.java
+++ src/de/craebby/aquaplan/view/TankOverviewController.java
@@ -2,7 +2,10 @@ package de.craebby.aquaplan.view;
 
 import de.craebby.aquaplan.MainApp;
 import de.craebby.aquaplan.model.Tank;
+import de.craebby.aquaplan.util.DateUtil;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -51,9 +54,92 @@ public class TankOverviewController {
 		nameCol.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
 		locationCol.setCellValueFactory(cellData -> cellData.getValue().locationProperty());
 		sizeCol.setCellValueFactory(cellData -> cellData.getValue().sizeProperty());
+		
+		//Clear tank details
+		showTankDetails(null);
+		
+		
+		//Listen for selection changes
+		tankTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showTankDetails(newValue));
 	}
 	
-	//Called by MainApp to give rederence back to itself
+	
+	private void showTankDetails(Tank tank) {
+		if (tank != null) {
+			//Fill the labels from the tank object
+			nameLabel.setText(tank.getName());
+			locationLabel.setText(tank.getLocation());
+			sizeLabel.setText(Integer.toString(tank.getSize()));
+			dimLabel.setText(tank.getDimensions());
+			lightLabel.setText(tank.getLight());
+			filterLabel.setText(tank.getFilter());
+			heaterLabel.setText(tank.getHeater());
+			operatingLabel.setText(DateUtil.format(tank.getOperatingDate()));
+		} else {
+			nameLabel.setText("");
+			locationLabel.setText("");
+			sizeLabel.setText("");
+			dimLabel.setText("");
+			lightLabel.setText("");
+			filterLabel.setText("");
+			heaterLabel.setText("");
+			operatingLabel.setText("");
+		}
+		
+		
+		
+	}
+	
+	@FXML
+	private void handleDeleteTank() {
+		int selectedIndex = tankTable.getSelectionModel().getSelectedIndex();
+		if (selectedIndex >=0) {
+			tankTable.getItems().remove(selectedIndex);
+		} else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.initOwner(mainApp.getPrimaryStage());
+			alert.setTitle("Keine Auswahl");
+			alert.setHeaderText("Kein Aquarium ausgewählt");
+			alert.setContentText("Bitte wähle ein Aquarium in der Liste aus.");
+			alert.showAndWait();
+		}
+	}
+	
+	@FXML
+	private void handleNewTank() {
+	    Tank tempTank = new Tank();
+	    boolean okClicked = mainApp.showTankEditDialog(tempTank);
+	    if (okClicked) {
+	        mainApp.getTankData().add(tempTank);
+	    }
+	}
+
+	/**
+	 * Called when the user clicks the edit button. Opens a dialog to edit
+	 * details for the selected person.
+	 */
+	@FXML
+	private void handleEditTank() {
+	    Tank selectedTank = tankTable.getSelectionModel().getSelectedItem();
+	    if (selectedTank != null) {
+	        boolean okClicked = mainApp.showTankEditDialog(selectedTank);
+	        if (okClicked) {
+	            showTankDetails(selectedTank);
+	        }
+
+	    } else {
+	        // Nothing selected.
+	        Alert alert = new Alert(AlertType.WARNING);
+	        alert.initOwner(mainApp.getPrimaryStage());
+	        alert.setTitle("Keine Auswahl");
+	        alert.setHeaderText("Kein Aquarium ausgewählt");
+	        alert.setContentText("Wähle ein Aquarium aus der Liste.");
+
+	        alert.showAndWait();
+	    }
+	}
+	
+	//Called by MainApp to give reference back to itself
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
 		
